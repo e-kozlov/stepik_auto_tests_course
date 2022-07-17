006aa88c478256d80c6d474004a15184d0258f33
e-kozlov <e-kozlov@list.ru>
2022-07-18 02:48:51 +0400

    New tests

---
+++ Block 3/unittest_examine.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import unittest
+
+
+class TestClassOne(unittest.TestCase):
+
+    def test_regression_1(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля
+            first_name = browser.find_element(By.XPATH, "//input[contains(@placeholder, 'first name')]")
+            first_name.send_keys('Random')
+            last_name = browser.find_element(By.XPATH, "//input[contains(@placeholder, 'last name')]")
+            last_name.send_keys('Random')
+            email = browser.find_element(By.XPATH, "//input[contains(@placeholder, 'mail')]")
+            email.send_keys('Random')
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(3)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+    def test_regression_2(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+
+            # Ваш код, который заполняет обязательные поля
+            first_name = browser.find_element(By.XPATH, "//input[contains(@placeholder, 'first name')]")
+            first_name.send_keys('Random')
+            last_name = browser.find_element(By.XPATH, "//input[contains(@placeholder, 'last name')]")
+            last_name.send_keys('Random')
+            email = browser.find_element(By.XPATH, "//input[contains(@placeholder, 'mail')]")
+            email.send_keys('Random')
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(3)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
